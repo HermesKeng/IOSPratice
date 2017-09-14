44266dea3b2441403e6bc800fb3d75727c48fcc1
耿楷寗 <keng@koukais-MacBook-Air.local>
2017-09-14 12:54:20 +0800

    CustomTableView

---
+++ Foodpin/Foodpin/RestaurantTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantTableViewCell.swift
+//  Foodpin
+//
+//  Created by 耿楷寗 on 14/09/2017.
+//  Copyright © 2017 耿楷寗. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
